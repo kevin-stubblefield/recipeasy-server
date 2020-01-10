3c79c65a0b255f96c749cf78171994378fed63ed
Kevin Stubblefield <kevin@stubblefield.io>
2020-01-09 20:06:38 -0600

    Add database to gitignore so it doesn't get committed. Add db creation to init file

---
+++ init.js
@@ -0,0 +1,23 @@
+const DAO = require('./db/dao.js');
+const Recipes = require('./db/recipeRepository.js');
+const IngredientGroups = require('./db/ingredientGroupRepository.js');
+const Ingredients = require('./db/ingredientRepository');
+const Instructions = require('./db/instructionRepository');
+const NutritionInfo = require('./db/nutritionRepository');
+
+async function init() {
+    const dao = new DAO('./database.rec');
+    const recipes = new Recipes(dao);
+    const ingredientGroups = new IngredientGroups(dao);
+    const ingredients = new Ingredients(dao);
+    const instructions = new Instructions(dao);
+    const nutritionInfo = new NutritionInfo(dao);
+
+    await recipes.createTable();
+    await ingredientGroups.createTable();
+    await ingredients.createTable();
+    await instructions.createTable();
+    await nutritionInfo.createTable();
+}
+
+init();
