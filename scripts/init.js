aef7c863a7ca08fa37bbdd8d6fe36dece04bd9f4
Kevin Stubblefield <kevin@stubblefield.io>
2020-01-09 20:22:42 -0600

    Update file paths in init

--- scripts/init.js
+++ scripts/init.js
@@ -1,12 +1,12 @@
-const DAO = require('./db/dao.js');
-const Recipes = require('./db/recipeRepository.js');
-const IngredientGroups = require('./db/ingredientGroupRepository.js');
-const Ingredients = require('./db/ingredientRepository');
-const Instructions = require('./db/instructionRepository');
-const NutritionInfo = require('./db/nutritionRepository');
+const DAO = require('../db/dao.js');
+const Recipes = require('../db/recipeRepository.js');
+const IngredientGroups = require('../db/ingredientGroupRepository.js');
+const Ingredients = require('../db/ingredientRepository');
+const Instructions = require('../db/instructionRepository');
+const NutritionInfo = require('../db/nutritionRepository');
 
 async function init() {
-    const dao = new DAO('./database.rec');
+    const dao = new DAO('../database.rec');
     const recipes = new Recipes(dao);
     const ingredientGroups = new IngredientGroups(dao);
     const ingredients = new Ingredients(dao);
