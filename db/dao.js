bdcdc4f559dc2fd256019f7396ea3129f1938c53
Kevin Stubblefield <kevin@stubblefield.io>
2020-01-11 14:08:03 -0600

    Add get and all functions to dao

--- db/dao.js
+++ db/dao.js
@@ -15,7 +15,7 @@ class DAO {
         return new Promise((resolve, reject) => {
             this.db.run(sql, params, function(err) {
                 if (err) {
-                    console.log('Error running sql ' + sql);
+                    console.log(`Error running sql: ${sql}`);
                     console.log(err);
                     reject(err);
                 } else {
@@ -23,6 +23,34 @@ class DAO {
                 }
             });
         });
+    }
+
+    get(sql, params = []) {
+        return new Promise((resolve, reject) => {
+            this.db.get(sql, params, (err, result) => {
+                if (err) {
+                    console.log(`Error running sql: ${sql}`);
+                    console.log(err);
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+    all(sql, params = []) {
+        return new Promise((resolve, reject) => {
+            this.db.all(sql, params, (err, results) => {
+                if (err) {
+                    console.log(`Error running sql: ${sql}`);
+                    console.log(err);
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
+        })
     }
 
     serialize(callback) {
