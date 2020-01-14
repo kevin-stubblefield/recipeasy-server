c1cbab11c6c785764c9ffeb396fc983fee1f7cfa
Kevin Stubblefield <kevin@stubblefield.io>
2020-01-13 19:57:23 -0600

    Remove console output of recipes whenever the routes are called

--- routes/recipes.js
+++ routes/recipes.js
@@ -5,7 +5,7 @@ router.get('/', async (req, res) => {
     // will probably make an orm class that can handle the object mapping to move it out of the controller
     const recipeController = new RecipeController();
     const recipes = await recipeController.getAllRecipes();
-    console.log(recipes);
+    // console.log(recipes);
     return res.render('pages/index', { title: 'Recipe Catalog', recipes: recipes });
 });
 
@@ -13,7 +13,7 @@ router.get('/:id', async (req, res) => {
     // will probably make an orm class that can handle the object mapping to move it out of the controller
     const recipeController = new RecipeController();
     const recipe = await recipeController.getRecipeById(req.params.id);
-    console.log(recipe);
+    // console.log(recipe);
     return res.render('pages/recipe', { title: recipe.name, recipe: recipe });
 });
 
