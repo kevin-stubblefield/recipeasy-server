1c35dc778ffa5d8012e111d8015ccfe5bed4a482
Kevin Stubblefield <kevin@stubblefield.io>
2020-01-13 18:42:27 -0600

    Add fetching of individual recipe to controller. Will definitely be refactored in the future. Made recipe detail page for a starting point

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -20,7 +20,13 @@ class RecipeController {
         return recipes;
     }
 
-    async buildRecipesObject(recipes) {
+    async getRecipeById(id) {
+        const recipe = await this.recipeRepository.fetchById(id);
+        const result = this.buildRecipeObject(recipe);
+        return result;
+    }
+
+    async buildRecipeObjects(recipes) {
         let results = [];
         for (const recipe of recipes) {
             recipe.ingredientGroups = await this.ingredientGroupsRepository.fetchByRecipeId(recipe.id);
@@ -34,6 +40,18 @@ class RecipeController {
             results.push(recipe);
         }
         return results;
+    }
+
+    async buildRecipeObject(recipe) {
+        recipe.ingredientGroups = await this.ingredientGroupsRepository.fetchByRecipeId(recipe.id);
+        const ingredientGroupIds = recipe.ingredientGroups.map((ingredientGroup) => ingredientGroup.id);
+        const ingredients = await this.ingredientsRepository.fetchByIngredientGroupIds(ingredientGroupIds);
+        for (const ingredientGroup of recipe.ingredientGroups) {
+            ingredientGroup.ingredients = ingredients.filter((ingredient) => ingredient.ingredientGroupId === ingredientGroup.id);
+        }
+        recipe.instructions = await this.instructionsRepository.fetchByRecipeId(recipe.id);
+        recipe.nutritionInfo = await this.nutritionInfoRepository.fetchByRecipeId(recipe.id);
+        return recipe;
     }
 }
 
